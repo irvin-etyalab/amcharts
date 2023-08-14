6df1361cc58d5e3464b700bb36bf5d7f9733c01c
Irvin Meneses <irvin@etyalab.com>
2023-08-14 12:16:26 -0500

    initial commit

---
+++ src/components/CustomChart.vue
@@ -0,0 +1,50 @@
+<template>
+  <div style="padding: 24px">
+    <div ref="chartdiv" id="chartDiv" />
+  </div>
+</template>
+
+<script>
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
+export default {
+  name: "CustomChart",
+  data() {
+    return {
+      chart: null,
+    }
+  },
+  props: {
+    config: Object,
+  },
+  watch: { 
+    config: function(newVal, oldVal) {
+      console.log('Prop changed: ', newVal, ' | was: ', oldVal)
+      this.createChart();
+    }
+  },
+  mounted() {
+    am4core.useTheme(am4themes_animated);
+  },
+  methods: {
+    createChart() {
+      if (this.chart) {
+        this.chart.dispose();
+      }
+
+      if(this.config !== null) {
+        this.chart = am4core.createFromConfig(this.config, this.$refs.chartdiv, am4charts[this.config.type]);
+      }
+    }
+  },
+}
+</script>
+
+<style scoped>
+  #chartDiv {
+    width: 50%;
+    height: 400px;
+  }
+</style>
---
+++ src/components/HomeView.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <select v-model="selectedChartType" @change="updateChartConfig">
+      <option value="line">Line</option>
+      <option value="bar">Bar</option>
+      <option value="column">Column</option>
+      <option value="pie">Pie</option>
+      <option value="donut">Donut</option>
+    </select>
+    <custom-chart :config="config" />
+    <v-jsoneditor v-if="config" v-model="config" height="300px" />
+  </div>
+</template>
+
+<script>
+import CustomChart from './CustomChart.vue'
+import json from '../data/charts.json'
+import VJsoneditor from 'v-jsoneditor/src/index'
+
+export default {
+  components: { CustomChart, VJsoneditor },
+  name: 'HomeView',
+  data() {
+    return {
+      config: null,
+      selectedChartType: 'line',
+      configJson: json
+    }
+  },
+  mounted() {
+    this.config = this.configJson[this.selectedChartType];
+  },
+  methods: {
+    updateChartConfig(e) {
+      const chartType = e.target.value;
+      this.config = this.configJson[chartType];
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
